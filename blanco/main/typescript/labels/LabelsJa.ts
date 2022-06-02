21cdcb26222d0afedfcd8270f1c9403b61cb0d9f
haino <haino@ueo>
2022-06-02 18:51:31 +0900

    regenerate source code of LabelsJa

--- blanco/main/typescript/labels/LabelsJa.ts
+++ blanco/main/typescript/labels/LabelsJa.ts
@@ -1,7 +1,38 @@
 import {LocaleLabels} from "@/i18n/LocaleLabels";;
+import { ChartSampleJa } from "./ChartSampleJa";
 
 /**
  * Labels Class for i18n
  */
 export class LabelsJa extends LocaleLabels {
+    /**
+     * サンプル図ページで使用するラベルを定義します。
+     *
+     * フィールド: [ChartSample]。
+     * デフォルト: [new ChartSampleJa()]。
+     */
+    private fChartSample: ChartSampleJa = new ChartSampleJa();
+
+    /**
+     * フィールド [ChartSample] の値を設定します。
+     *
+     * フィールドの説明: [サンプル図ページで使用するラベルを定義します。]。
+     *
+     * @param argChartSample フィールド[ChartSample]に設定する値。
+     */
+    set ChartSample(argChartSample: ChartSampleJa) {
+        this.fChartSample = argChartSample;
+    }
+
+    /**
+     * フィールド [ChartSample] の値を取得します。
+     *
+     * フィールドの説明: [サンプル図ページで使用するラベルを定義します。]。
+     * デフォルト: [new ChartSampleJa()]。
+     *
+     * @return フィールド[ChartSample]から取得した値。
+     */
+    get ChartSample(): ChartSampleJa {
+        return this.fChartSample;
+    }
 }
