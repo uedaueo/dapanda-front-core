93c6a8c3b7365c94fc96b9a1a2b1aa6210bac696
tueda <ueo@ueo.co.jp>
2022-06-10 06:35:18 +0900

    Test for message customization of built-in rules.

--- src/validate/ValidateMessageEn.ts
+++ src/validate/ValidateMessageEn.ts
@@ -8,4 +8,5 @@ import { AbstractLocaleMessageObject } from "@/i18n/AbstractLocaleMessageObject"
  */
 export class ValidateMessageEn extends AbstractLocaleMessageObject {
   dapanda: string = "The true {field} means 0:{dapanda} .";
+  required: string = "How many times do I have to say, {field} is REQUIRED!";
 }
--- src/validate/ValidateMessageJa.ts
+++ src/validate/ValidateMessageJa.ts
@@ -8,4 +8,5 @@ import { AbstractLocaleMessageObject } from "@/i18n/AbstractLocaleMessageObject"
  */
 export class ValidateMessageJa extends AbstractLocaleMessageObject {
   dapanda: string = "真 {field} とは 0:{dapanda} のことである。";
+  required: string = "{field} は必須だとあれほど、、、";
 }
