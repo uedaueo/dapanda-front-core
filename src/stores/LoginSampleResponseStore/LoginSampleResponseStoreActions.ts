0aa65260136eb5d19abe96d2bdef2fd7da646ccd
taihei <taihei@ueo.co.jp>
2024-10-30 21:56:30 +0900

    fix typo.

--- src/stores/LoginSampleResponseStore/LoginSampleResponseStoreActions.ts
+++ src/stores/LoginSampleResponseStore/LoginSampleResponseStoreActions.ts
@@ -4,7 +4,7 @@ import {
 import {CommonResponse} from "%/blanco/restgenerator/valueobject/CommonResponse";
 
 export const loginSampleResponseStoreActions = defineLoginSampleResponseStoreActions({
-    setResponse(respnse: CommonResponse): void {
-        this.response = respnse;
+    setResponse(response: CommonResponse): void {
+        this.response = response;
     }
 });
